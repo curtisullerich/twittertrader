17b40634e95bf1cabc2707615316412ae9040475
Daniel Stiner <danstiner@gmail.com>
2012-11-13 23:43:35 +0000

    server updates

--- server/routes/classified.js
+++ server/routes/classified.js
@@ -2,15 +2,46 @@ var tweetsdb = require('../store').tweets;
 var writeJSON = require('../io').writeJSON;
 var nconf = require('../config').nconf;
 var writeCSV = require('../io').writeCSV;
-
+var JSONStream = require('JSONStream');
 
 /*
  * POST classified listing.
  */
 
-exports.post = function(req, res){
-  res.send("respond with a resource");
+exports.post = function(req, res) {
+  var parser = JSONStream.parse([true]);
+  parser.on('data', function(data) {
+    var id_str = data.id_str;
+    var company = data.company;
+    var sentiment = data.sentiment;
+
+    tweetsdb.findOne({'id_str':id_str}, function(err, tweet) {
+
+      tweet.cstamp = data.cstamp;
+
+      if(company) {
+        tweet.company = company;
+        tweet.companyConfidence = 2;
+      }
+
+      if(sentiment) {
+        tweet.sentiment = sentiment;
+        tweet.sentimentConfidence = 2;
+      }
+
+      tweet.classification = "classified";
+    });
+//    console.dir(data);
+  });
+  parser.on('root', function(root, count) {
+    res.end();
+  });
+
+  req.pipe(parser);
 };
+
+
+
 
 /*
  * POST unclassify
@@ -24,11 +55,11 @@ exports.post_unclassify = function(req, res){
 
     tweetsdb.findOne({'id_str' : id}, function(err, tweet) {
 
-      if(field == 'company') {
+      if(field == 'company' && tweet['company']) {
         delete tweet.company;
         delete tweet.companyConfidence;
       }
-      else if(field == 'sentiment') {
+      else if(field == 'sentiment' && tweet['sentiment']) {
         delete tweet.sentiment;
         delete tweet.sentimentConfidence;
       }
@@ -71,6 +102,36 @@ exports.company_random = function(req, res) {
 	var tweets = tweetsdb.find({random:{$gte:Math.random()}, classification:"classified", companyConfidence:{$gte:lowerConf, $lte:upperConf}}).limit(count);
 	writeJSON(res, tweets);
 };
+
+
+exports.company_confidence_search = function(req, res) {
+	var confidence = req.params.confidence;
+	var count = parseInt(req.params.count);
+
+	var lowerConf = 0.0;
+        var upperConf = 1.0;
+
+	if(confidence[0] == '<') {
+		upperConf = parseFloat(confidence);
+	} else if(confidence[0] == '>') {
+		lowerConf = parseFloat(confidence);
+	} else {
+
+		try {
+			var confSplit = confidence.split(":");
+			lowerConf = parseFloat(confSplit[0]);
+			upperConf = parseFloat(confSplit[1]);
+		} catch(e) {
+			res.end("{\"error\":\"Invalid confidence param\"");
+			return;
+	}
+
+	}
+
+	var tweets = tweetsdb.find({company:req.params.company, text:{$regex:req.params.company}, classification:"classified", companyConfidence:{$gte:lowerConf, $lte:upperConf}}).limit(count);
+	writeJSON(res, tweets);
+};
+
 
 
 exports.company_confidence = function(req, res) {
