e662f990f42da0ff0887ef1489536376b57223dd
Daniel Stiner <danstiner@gmail.com>
2012-11-25 16:11:46 -0600

    basic classifier2

--- server/classifier.js
+++ server/classifier.js
@@ -1,10 +1,10 @@
 var tweetsdb = require('store').tweets;
-var classifier = require('class');
+var classs = require('class');
 
 var batchSize = 1000;
 
 
-classifier.setup(function (classification) {
+var classifier = classs.setup(function (classification) {
 	tweetsdb.update(
 		{ id_str : classification.id_str },
 		{ $set : classification }
@@ -14,30 +14,13 @@ classifier.setup(function (classification) {
 while(true) {
 	var tweets = tweetsdb.find({classification:{$exists:false}}).limit(batchSize);
 
-	
+	// Stream tweets from database
+	var tstream = tweets.cursor.stream();
+	tstream.on('data', function (tweet) {
+	  classifier.classify(tweet);
+	});
+	tstream.on('close', function () {
+	});
 }
 
-var db = require("mongojs").connect(nconf.get('database'), [nconf.get('collection')]);
-
-var nTwitter = require("ntwitter");
-var twitter = new nTwitter(nconf.get('twitter_auth'));
-
-var i=0;
-twitter.stream(
-	'statuses/filter',
-	{ track: nconf.get('keywords') },
-	function(stream) {
-		stream.on('data', function(tweet) {
-			if(i > nconf.get('capture:frequency') && tweet.user.lang == 'en')
-			{
-				i = 0;
-				tweet.random = Math.random();
-				tweet.created_at = new Date(tweet.created_at);
-				db.twits.save(tweet);
-				console.log('tweet: '+ tweet.created_at);
-			}
-			i++;
-		});
-	}
-);
-
+classifier.end();
