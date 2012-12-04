7102c045c22614d35d6908b3caa4e7e5237c2b11
Curtis Ullerich <curtisullerich@gmail.com>
2012-12-04 02:36:20 -0600

    major updates to paper, added parser to Main

--- classify/src/main/java/classification/Main.java
+++ classify/src/main/java/classification/Main.java
@@ -1,9 +1,34 @@
 package classification;
 
+import java.io.File;
 import java.io.IOException;
+import java.util.Scanner;
 
 public class Main {
 	public static void main(String... args) throws IOException {
+
+		Scanner s = new Scanner(new File("/home/curtis/Dropbox/Shared Folders/apple/allsorted"));
+		int i = 0;
+		while (s.hasNextLine()) {
+			String l = s.nextLine();
+
+			String ls[] = l.split("\t");
+			if (i == 0) {
+				System.out.print(ls[1]);
+			} 
+				System.out.print("\t" + ls[2]);
+			if (i == 17) {
+				if (!ls[0].equals("1800")) {
+					System.out.println("FAIL");
+					System.exit(1);
+				}
+				System.out.print("\n");
+				i = -1;
+			} 
+
+			i++;
+
+		}
 		// ModelUpdater m = new ModelUpdater();
 		// Classifier c = m.createNewCompanyWikipediaModel();
 
